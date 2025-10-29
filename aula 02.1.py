3729d3c4ba03d06ddb23e12d93fc2d423a7f8c18
Jordson1 <jordsonemerson.barbosa@gmail.com>
2025-10-28 21:03:43 -0300

    Add files via upload

---
+++ aula 02.1.py
@@ -0,0 +1,106 @@
+#x=5 
+#Y=2 *x 
+#x= x+Y
+#print (x,Y)
+#a=10 
+#print(a)
+#b=20
+#print(b)
+#a=b 
+#print(a)
+#b=a+5 
+#print(b)
+#print (a,b)
+#Algoritmo da média
+#nota1=6
+#nota2= 9
+#media= (nota1 + nota2) /2
+#print ('média: ', media)
+#numero= 3 
+#numero= numero+1
+#numero= numero*2
+#print(numero)
+
+#Entrada e saída de ação
+#nome= input('digite seu nome:')
+#idade= int(input('digite sua idade:'))
+#print('olá,', nome,'! Você tem', idade, 'anos.')
+
+#Desafio das variáveis interligadas
+#a=2 
+#b=a+3
+#c=a+b
+#a=c-1
+#print(a,b,c)
+
+#Atividade de raciocínios com valores boleanos
+#x=10
+#y=5
+#resultado= x>y
+#print(resultado)
+
+#nome= input('digite seu nome')
+#idade=input('DIGITE SUA IDADE:')
+#print('olá,',nome, '! você tem', idade, 'anos')
+
+#x=8 
+#y=3
+#z= 8
+#teste1= x== z
+#teste2= y>=x
+#teste3= x  = y
+#print(teste1, teste2, teste3)
+
+#x=8
+#y= 4
+#z= 3 
+#teste1= x==z
+#teste2=y>x
+#teste3= y<x
+#print(teste1, teste2, teste3)
+
+#nome=input('digite seu nome:')
+#idade= float(input('digite sua idade:'))
+#print(type(nome))
+#print (type(idade))
+#print('nome->',nome, 'idade:')
+
+#a=2
+#b= a+3
+#c= a+b
+#a= c-1 
+#print('---------------------------------------')
+#print('Valor a:',a,'B:',b,'C:',c)
+#print('---------------------------------------')
+
+#nome= input('digite seu nome:')
+#nota1= float(input('digite a primeira nota:'))
+#nota2= float(input('digite a segunda nota:'))
+#media= (nota1+nota2)/2
+#if media >= 7:
+# print('Parabéns, você foi aprovado!')
+#else:
+# print('Infelizmente você foi reprovado!')
+
+#O enigma das operações 
+# x=2 
+# y= x**3
+# x= y-4 
+# print(x+y)
+
+# texto= 'python'
+# numero=0
+# num_negativo= -8
+# texto_vazio=''
+
+# print('texto: ', bool(texto))
+# print('valor zero: ',bool(numero))
+# print('numero negativo :', bool(num_negativo))
+# print('texto vazio:', bool(texto_vazio))
+
+# a= True
+# b= False
+# c= True 
+# resultado= (a and c ) or b 
+# print('resultado:', resultado)
+# print(bool(False))
